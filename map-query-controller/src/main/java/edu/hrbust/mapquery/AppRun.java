cfc7b0a832474787fce484f7d1c1cd938c818df3
ar2oria <ar2oria@DESKTOP-I86RRS9>
2019-03-12 00:52:08 +0800

    init

---
+++ map-query-controller/src/main/java/edu/hrbust/mapquery/AppRun.java
@@ -0,0 +1,17 @@
+package edu.hrbust.mapquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @describe:
+ * @package: edu.hrbust.mapquery
+ * @dateTime: 2019/3/12 0:50
+ * @author: ar2oria
+ */
+
+@SpringBootApplication
+public class AppRun {
+	public static void main(String[] args) {
+		SpringApplication.run(AppRun.class, args);
+	}
+}
