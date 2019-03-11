badefe834c9f3af514bfb2151a3d99cb81a1d6ea
ar2oria <ar2oria@DESKTOP-I86RRS9>
2019-03-12 01:11:24 +0800

    init

---
+++ map-query-controller/src/main/java/edu/hrbust/mapquery/controller/HealthCheck.java
@@ -0,0 +1,20 @@
+package edu.hrbust.mapquery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @describe:
+ * @package: edu.hrbust.mapquery.controller
+ * @dateTime: 2019/3/12 0:56
+ * @author: ar2oria
+ */
+@RestController
+public class HealthCheck {
+	@RequestMapping("/healthCheck")
+	public String healthCheck(){
+		return "ok";
+	}
+}
